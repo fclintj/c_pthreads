647a4d255dcd8d0929a834432c1bc2da98451a67
clint <clint.ferrin@gmail.com>
2017-06-26 15:45:04 -0600

    Refactored

--- multiplication.c
+++ multiplication.c
@@ -20,6 +20,64 @@ struct thread_info {
     struct mat *mat_result;
 };
 
+void make_mat(int rows, int cols, struct mat* arr);
+void* multiply_with_threads(void* arg);
+void multiply(struct mat* arr, struct mat* arr2, struct mat* result );
+void fill_mat(struct mat* arr);
+int sum_array(int* arr, int size);
+
+int main() {
+    int col=1000;
+    int row=1000;
+    int num_threads = NUMTHREADS;
+    struct thread_info mat_info[num_threads];
+    pthread_t thread[num_threads];
+    struct mat arr, arr2, arr_result;
+    make_mat(row,col,&arr);
+    make_mat(row,col,&arr2);
+    make_mat(row,col,&arr_result);
+    fill_mat(&arr); 
+    fill_mat(&arr2); 
+
+    int rows_per_thread = ceil(double(row) / num_threads);
+    printf("%d",rows_per_thread);
+
+    // store information for threads object
+    for (int i = 0; i < num_threads; i++) {
+        mat_info[i].mat_a = &arr;
+        mat_info[i].mat_b = &arr2;
+        mat_info[i].mat_result = &arr_result;
+        mat_info[i].is_running = 1;
+        mat_info[i].start = i*rows_per_thread;
+        mat_info[i].stop = (i+1)*rows_per_thread;
+    }
+    mat_info[num_threads-1].stop=row;
+
+    /* print_mat(&arr); */
+    for (int i=0; i<num_threads; i++) {
+        pthread_create(&thread[i], NULL, multiply_with_threads, &mat_info[i]);
+    }
+
+    /* Wait for Threads to Finish */
+    int flags[num_threads];
+    while(sum_array(flags,num_threads)!=3){
+        for (int i = 0; i < num_threads; i++) {
+            if(mat_info[i].is_running == 0 && flags[i] != 1){ 
+                printf("done\n");
+                flags[i]=1;
+            }
+        }
+
+    }
+
+    /* multiply(&arr,&arr2,&arr_result); */    
+    
+    free(arr.mat);
+    free(arr2.mat);
+    free(arr_result.mat);
+
+    return 0;
+}
 
 void make_mat(int rows, int cols, struct mat* arr){
     arr->mat = (int *)malloc(rows * cols * sizeof(int));
@@ -92,57 +150,4 @@ int sum_array(int* arr, int size){
        sum += arr[i]; 
     }
     return sum;
-}
-
-int main() {
-    int col=1000;
-    int row=1000;
-    int num_threads = NUMTHREADS;
-    struct thread_info mat_info[num_threads];
-    pthread_t thread[num_threads];
-    struct mat arr, arr2, arr_result;
-    make_mat(row,col,&arr);
-    make_mat(row,col,&arr2);
-    make_mat(row,col,&arr_result);
-    fill_mat(&arr); 
-    fill_mat(&arr2); 
-
-    int rows_per_thread = ceil(double(row) / num_threads);
-    printf("%d",rows_per_thread);
-
-    // store information for threads object
-    for (int i = 0; i < num_threads; i++) {
-        mat_info[i].mat_a = &arr;
-        mat_info[i].mat_b = &arr2;
-        mat_info[i].mat_result = &arr_result;
-        mat_info[i].is_running = 1;
-        mat_info[i].start = i*rows_per_thread;
-        mat_info[i].stop = (i+1)*rows_per_thread;
-    }
-    mat_info[num_threads-1].stop=row;
-
-    /* print_mat(&arr); */
-    for (int i=0; i<num_threads; i++) {
-        pthread_create(&thread[i], NULL, multiply_with_threads, &mat_info[i]);
-    }
-
-    /* Wait for Threads to Finish */
-    int flags[num_threads];
-    while(sum_array(flags,num_threads)!=3){
-        for (int i = 0; i < num_threads; i++) {
-            if(mat_info[i].is_running == 0 && flags[i] != 1){ 
-                printf("done\n");
-                flags[i]=1;
-            }
-        }
-
-    }
-
-    /* multiply(&arr,&arr2,&arr_result); */    
-    
-    free(arr.mat);
-    free(arr2.mat);
-    free(arr_result.mat);
-
-    return 0;
 }
